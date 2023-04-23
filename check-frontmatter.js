59b1952f31c4f28262c0b5d01f12eab405f666c7
Thiago Murtinho <thiago.murtinho@live.com>
2023-04-22 23:58:38 -0300

    Actions changed

--- check-frontmatter.js
+++ check-frontmatter.js
@@ -9,8 +9,30 @@ async function processFiles(files) {
 
 const modifiedFiles = process.argv.slice(2);
 
-const { CHANGED_FILES } = process.env;
+const { added_files,
+  copied_files,
+  deleted_files,
+  modified_files,
+  renamed_files,
+  type_changed_files,
+  unmerged_files,
+  unknown_files,
+  all_changed_and_modified_files,
+  all_changed_files,
+  all_modified_files, } = process.env;
 
-console.log('envs:', JSON.stringify(JSON.parse(CHANGED_FILES), null , 2))
+console.log('envs:', JSON.stringify({
+  added_files,
+copied_files,
+deleted_files,
+modified_files,
+renamed_files,
+type_changed_files,
+unmerged_files,
+unknown_files,
+all_changed_and_modified_files,
+all_changed_files,
+all_modified_files,
+}, null , 2))
 
 processFiles(modifiedFiles);
